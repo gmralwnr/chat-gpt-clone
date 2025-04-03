d303fd8de36a88199e8efbba269293a3b248e630
kmj0701 <kmj0701@coremethod.co.kr>
2025-04-03 18:22:23 +0900

    이미지 확장자 변경

--- chatgpt-clone2/components/chat/Logo.tsx
+++ chatgpt-clone2/components/chat/Logo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export function Logo() {
     return <Link href={BASE_URL} className="flex items-center gap-2">
         {/* 퍼블릭은 생략가능   */}
-        <Image width={90} height={90} src="/logo4.png" alt="logo"></Image>
+        <Image width={90} height={90} src="/logo100.PNG" alt="logo"></Image>
 
 
     </Link>
